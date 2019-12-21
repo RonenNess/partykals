7e2f4bb66cb3111968a472c68bc6c32d733e506d
Ronen <RonenNess@gmail.com>
2019-12-21 21:59:05 +0200

    built new version

--- dist/partykals.js
+++ dist/partykals.js
@@ -1,8 +1,8 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Partykals = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // This file is just a wrapper to get THREE base module.
 // If its not in your global space, edit this file accordingly.
-module.exports = window.THREE;
-},{}],2:[function(require,module,exports){
+module.exports = window.THREE || require('three');
+},{"three":undefined}],2:[function(require,module,exports){
 /**
  * Implement an emitter class to generate particles.
  * Author: Ronen Ness.
@@ -838,6 +838,14 @@ class ParticlesSystem
     }
 
     /**
+     * Dispose the entire system.
+     */
+    dispose()
+    {
+        this.particlesGeometry.dispose();
+    }
+
+    /**
      * Did finish emitting?
      */
     get finished()
@@ -1072,6 +1080,7 @@ class ParticlesSystem
 
 // export the particles system
 module.exports = ParticlesSystem;
+
 },{"./_three":1,"./material/material":4,"./particle":7}],9:[function(require,module,exports){
 /**
  * Generate vectors within a 3d box.
